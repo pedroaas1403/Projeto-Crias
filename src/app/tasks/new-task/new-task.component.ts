5f0f4496c8b57a28d1e49399317633b1312a8a4c
Pedro Azevedo <jclip2005@gmail.com>
2026-04-07 12:40:00 -0300

    teste

---
+++ src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,40 @@
+import {Component, EventEmitter, HostListener, inject, Input, Output} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {type NewTaskData} from "../task/task.model";
+import {TasksService} from "../tasks.service";
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css'
+})
+export class NewTaskComponent {
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+  private taskService = inject(TasksService);
+
+  @HostListener('document:keydown.escape')
+  onEscapeKey() {
+    this.onCancel();
+  }
+
+  onCancel() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    this.taskService.addTask({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    }, this.userId);
+    this.close.emit();
+  }
+}
